dcc7cbb6acd24de95b4bde367a9f498c2a2c48c5
Jonny <jonnym22@gmail.com>
2020-09-05 08:32:59 +0100

    p92: add WasRun class and get it passing

--- part2/main.py
+++ part2/main.py
@@ -1,5 +1,14 @@
-test = WasRun("testMethod")
-print test.wasRun
-test.testMethod()
-print test.wasRun
 
+
+class WasRun:
+    def __init__(self, name):
+        self.wasRun = None
+
+    def test_method(self):
+        self.wasRun = 1
+
+
+test = WasRun("testMethod")
+print(test.wasRun)
+test.test_method()
+print(test.wasRun)
