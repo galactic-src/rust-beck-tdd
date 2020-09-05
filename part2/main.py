a83627424b029a86901feb9f85fd7be6a2c8bff1
Jonny <jonnym22@gmail.com>
2020-09-05 08:46:10 +0100

    p93: use reflection

--- part2/main.py
+++ part2/main.py
@@ -3,12 +3,17 @@
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
+        self.name = name
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
 
     def test_method(self):
         self.wasRun = 1
 
 
-test = WasRun("testMethod")
+test = WasRun("test_method")
 print(test.wasRun)
-test.test_method()
+test.run()
 print(test.wasRun)
