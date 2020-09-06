2e496fa61ba4e316ad73b9215f4de6c30f14a355
Jonny <jonnym22@gmail.com>
2020-09-06 09:34:31 +0100

    p108: add summary. and test that requires we handle exceptions

--- part2/main.py
+++ part2/main.py
@@ -1,4 +1,14 @@
 
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+
+    def test_started(self):
+        self.runCount += 1
+
+    def summary(self):
+        return "{} run, 0 failed".format(self.runCount)
+
 
 class TestCase:
     def __init__(self, name):
@@ -11,10 +21,13 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.test_started()
         self.set_up()
         method = getattr(self, self.name)
         method()
         self.tear_down()
+        return result
 
 
 class WasRun(TestCase):
@@ -31,6 +44,9 @@ class WasRun(TestCase):
     def tear_down(self):
         self.log += "tear_down "
 
+    def test_broken_method(self):
+        raise Exception
+
 
 class TestCaseTest(TestCase):
     def set_up(self):
@@ -39,7 +55,19 @@ class TestCaseTest(TestCase):
     def test_template_method(self):
         test = WasRun("test_method")
         test.run()
-        assert ("set_up test_method tear_down " == test.log)
+        assert "set_up test_method tear_down " == test.log
+
+    def test_result(self):
+        test = WasRun("test_method")
+        result = test.run()
+        assert "1 run, 0 failed" == result.summary()
+
+    def test_failed_result(self):
+        test = WasRun("test_broken_method")
+        result = test.run()
+        assert "1 run, 1 failed" == result.summary()
 
 
 TestCaseTest("test_template_method").run()
+TestCaseTest("test_result").run()
+TestCaseTest("test_failed_result").run()
